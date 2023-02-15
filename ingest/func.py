18f3c9a46277fadf285ef5e9b286bb49f8736e8f
Evan Anderson <evana@vmware.com>
2023-02-15 15:41:50 -0800

    Implement event creation

--- ingest/func.py
+++ ingest/func.py
@@ -1,5 +1,16 @@
 from typing import Any
 
+from cloudevents.http import CloudEvent
+from cloudevents.conversion import to_binary, to_json
+import requests
+
 def main(req: Any):
-    # Your function implementation goes here
-    return "Hello World!"
+    # Use the same attributes for all events
+    attributes = dict(type="com.example.cityquery")
+
+    resp = []
+    for line in req.data:
+        line = line.strip()
+        resp.append(CloudEvent(attributes, {"city": line}))
+    
+    return 200, "\n-----\n".join([to_json(e) for e in resp])
