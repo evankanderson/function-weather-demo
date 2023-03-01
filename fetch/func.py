bea6e6ded0984553285c76e99661f58bae6e48a1
Evan Anderson <evana@vmware.com>
2023-03-01 15:25:37 -0800

    Declaration order matters /sigh

--- fetch/func.py
+++ fetch/func.py
@@ -4,6 +4,13 @@ from typing import Any
 import noaa_sdk
 from redis import Sentinel, Redis
 
+def initRedis():
+    if os.environ.get('REDIS_SENTINEL'):
+        return Sentinel([(os.environ['REDIS_SENTINEL'], 26379)])
+    if os.environ.get('REDIS_HOST'):
+        return Redis(host=os.environ['REDIS_HOST'])
+    return None
+
 noaa = noaa_sdk.NOAA()
 
 redis = initRedis()
@@ -20,9 +27,3 @@ def fetch(data: Any, attributes: dict):
     # Your function implementation goes here
     return None
 
-def initRedis():
-    if os.environ.get('REDIS_SENTINEL'):
-        return Sentinel([(os.environ['REDIS_SENTINEL'], 26379)])
-    if os.environ.get('REDIS_HOST'):
-        return Redis(host=os.environ['REDIS_HOST'])
-    return None
